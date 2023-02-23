336353b991e5a729b415db29c33d1d9e72b517c1
GIT Admin <alandpearson@gmail.com>
2023-02-23 23:25:50 +0000

    Fix burn_webpages.sh path to webpages.espfs

--- burn_webpages.sh
+++ burn_webpages.sh
@@ -4,4 +4,4 @@ echo "Compiling webpages into espfs file"
 rm webpages.espfs
 make webpages.espfs
 echo "Burning webpages to ESP8266 relay board"
-python3 $ESPTOOL --port $PORT  --baud 115200 write_flash 0x50000 webpages.espfs 
+python3 $ESPTOOL --port $PORT  --baud 115200 write_flash 0x50000 ./firmware/webpages.espfs 
